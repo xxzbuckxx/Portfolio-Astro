0970e40a3781cec1c362bc1e44d3fe5dd90e4f28
Zachary Traczyk <me@zacktraczyk.com>
2023-02-07 14:09:02 -0800

    fix typescript error

--- src/projects.ts
+++ src/projects.ts
@@ -1,4 +1,5 @@
-import type { Project } from './components/Project.astro';
+import type { ProjectType } from './components/Project.astro';
+import type { ProjectHeaderType } from './components/ProjectHeader.astro';
 
 import underDashboard from './assets/images/project-previews/UNDER-Dashboard.gif';
 
@@ -6,7 +7,10 @@ import bmTitle from './assets/images/project-previews/BM-title.jpg';
 import bmTutorial from './assets/images/project-previews/BM-tutorial.jpg';
 import bmDamage from './assets/images/project-previews/BM-damage.jpg';
 
-export const projects: Project[] = [
+const projectItems: Array<ProjectType | ProjectHeaderType> = [
+  {
+    text: 'Here are some recent projects I have been working on. Lately I have been focused on learning React.',
+  },
   {
     title: 'Under.io Metric System',
     tags: ['fullstack', 'nodejs', 'typescript', 'react', 'aws'],
@@ -23,4 +27,15 @@ export const projects: Project[] = [
     description:
       'A web game I designed with a global leaderboard, an upgrade shop, and account functionality to save progress.',
   },
+  {
+    text: 'While working at the Digital Scholar Initative, a maker space at University of California Santa Cruz, I was tasked with creating a display project.',
+  },
+  {
+    text: 'I also made some projects during the school year, mostly in C and C++.',
+  },
+  {
+    text: 'I like making video games too. Here is one I am particularly proud of.',
+  },
 ];
+
+export default projectItems;
