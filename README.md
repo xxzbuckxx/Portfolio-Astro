69c710de231e9a190d360493df48b1e5999e8107
Zack Traczyk <20495250+xxzbuckxx@users.noreply.github.com>
2023-12-20 13:31:58 -0600

    udpate projects

--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 
 All commands are run from the root of the project, from a terminal:
 
-| Command                | Action                                           |
-| :--------------------- | :----------------------------------------------- |
-| `yarn install`         | Installs dependencies                            |
-| `yarn dev`             | Starts local dev server at `localhost:3000`      |
-| `yarn build`           | Build your production site to `./dist/`          |
-| `yarn preview`         | Preview your build locally, before deploying     |
-| `yarn astro ...`       | Run CLI commands like `astro add`, `astro check` |
-| `yarn astro --help`    | Get help using the Astro CLI                     |
-
+| Command             | Action                                           |
+| :------------------ | :----------------------------------------------- |
+| `yarn install`      | Installs dependencies                            |
+| `yarn dev`          | Starts local dev server at `localhost:3000`      |
+| `yarn build`        | Build your production site to `./dist/`          |
+| `yarn preview`      | Preview your build locally, before deploying     |
+| `yarn astro ...`    | Run CLI commands like `astro add`, `astro check` |
+| `yarn astro --help` | Get help using the Astro CLI                     |
--- src/projects.ts
+++ src/projects.ts
@@ -1,6 +1,15 @@
 import type { ProjectType } from './components/Project.astro';
 import type { ProjectHeaderType } from './components/ProjectHeader.astro';
 
+// StudentStay
+import studentStayHomepage from './assets/images/project-previews/STUDENTSTAY-Homepage.jpg';
+import studentStayMap from './assets/images/project-previews/STUDENTSTAY-Map.jpg';
+import studentStayMatching from './assets/images/project-previews/STUDENTSTAY-Matching.jpg';
+import studentStayProfile from './assets/images/project-previews/STUDENTSTAY-Profile.jpg';
+
+// SlugFit
+import slugfitHeader from './assets/images/project-previews/SLUGFIT-Header.jpg';
+
 // Under
 import underDashboard from './assets/images/project-previews/UNDER-Dashboard.gif';
 
@@ -29,64 +38,81 @@ import rsqBoss from './assets/images/project-previews/RSQ-Boss-Fight.jpg';
 
 const projectItems: Array<ProjectType | ProjectHeaderType> = [
   {
-    text: 'Here are some recent projects I have been working on. Lately I have been focused on learning React.',
+    title: 'StudentStay',
+    technologies: [
+      'fullstack',
+      'nodejs',
+      'typescript',
+      'tailwindcss',
+      'nextjs',
+      'mapbox',
+      'supabase',
+    ],
+    previews: [studentStayHomepage, studentStayMap, studentStayMatching, studentStayProfile],
+    description:
+      'StudentStay is a startup idea I pursued with a buisness partner. Our goal was to help university students with their housing searches. We created a webapp with a zillow-like rental map and a roomate finder preferences page. Although we did not reach our MVP, I learned a lot about the startup process and how to work with a team.',
   },
   {
+    title: 'SlugFit Workout App',
+    technologies: ['fullstack', 'nodejs', 'typescript', 'React Native', 'supabase'],
+    previews: [slugfitHeader],
+    code: 'https://github.com/zacktraczyk/SlugFit',
+    description:
+      'SlugFit is a mobile app I helped build to create, track, and share custom workouts. I created the authentication pages, workout builder, and profile pages. I also handled all repository management including inital React Native scaffolding, Git Flow setup, CI/CD pipelines with Github Actions, and Supabase configuration.',
+  },
+  // {
+  //   title: ''
+  // }
+  {
+    title: 'Flocking Simulation',
+    technologies: ['nodejs', 'typescript', 'threejs'],
+    previews: [boidDemo, boidDebug, boidFog, boidUi, boidOcean],
+    code: 'https://github.com/zacktraczyk/Boid-Simulation',
+    demo: 'https://zacktraczyk.github.io/Boid-Simulation/',
+    description:
+      'This flocking simulation was presented at the Digital Scholarship Symposium at University of California on May 25th, 2023. Using a holographic display I implemented Boids, an algorithm that simulates flocking behavior. The project was written in Typescript using Three.js for rendering.',
+  },
+  {
+    title: 'Blob Mob',
+    technologies: ['fullstack', 'nodejs', 'typescript', 'react', 'firebase'],
+    previews: [bmTitle, bmTutorial, bmDamage],
+    code: 'https://github.com/zacktraczyk/Blob-Mob',
+    demo: 'https://blob-mob.vercel.app/',
+    description:
+      'Blob Mob is a web game I designed with a global leaderboard, an upgrade shop, and account functionality. The core of the game is made in Typescript while the UI elments are made in React.',
+  },
+  {
+    title: "Ralph's Space Quest",
+    technologies: ['GameMaker Studio 2'],
+    demo: 'https://xxzbuckxx.itch.io/ralphs-space-quest-demo',
+    previews: [rsqTitle, rsqLevelSelect, rsqLevel, rsqBoss],
+    description:
+      '"Ralph\'s Space Quest" is a demo 2d-platformer shooter set on an extraterrestrial planet. The game contains a level select, a single demo level, and a boss fight. I created the majority of the assets only outsourcing the background art.',
+  },
+];
+
+const _project_archive: Array<ProjectType | ProjectHeaderType> = [
+  {
     title: 'Under.io Metric System',
-    tags: ['fullstack', 'nodejs', 'typescript', 'react', 'aws'],
+    technologies: ['fullstack', 'nodejs', 'typescript', 'react', 'aws'],
     previews: [underDashboard],
     description:
       'At a startup, it is crucial to understand where the buisness is at. That is why I was tasked with building an admin dashboard that displayed important site metrics and allowed easy access of customer data.',
   },
   {
-    title: 'Blob Mob',
-    tags: ['fullstack', 'nodejs', 'typescript', 'react', 'firebase'],
-    previews: [bmTitle, bmTutorial, bmDamage],
-    code: 'https://github.com/xxzbuckxx/Blob-Mob',
-    demo: 'https://blob-mob.vercel.app/',
-    description:
-      'A web game I designed with a global leaderboard, an upgrade shop, and account functionality to save progress.',
-  },
-  {
-    text: 'While working at the Digital Scholar Initative, a maker space at University of California Santa Cruz, I was tasked with creating a display project.',
-  },
-  {
-    title: 'Flocking Simulation',
-    tags: ['nodejs', 'typescript', 'threejs'],
-    previews: [boidDemo, boidDebug, boidFog, boidUi, boidOcean],
-    code: 'https://github.com/xxzbuckxx/Boid-Simulation',
-    demo: 'https://xxzbuckxx.github.io/Boid-Simulation/',
-    description:
-      'Using the 3D visualization tools in the UCSC Science & Engineering DSI I made an interactive visualization. I replicated flocking behavior using Boids, an emerging phenomena and example of complexity theory. I plan to put this simulation in hologram technology we have at the space. I documented my progess as a guide and inspiration to students to pursue similar projects.',
-  },
-  {
-    text: 'I also made some projects during the school year, mostly in C and C++.',
-  },
-  {
-    title: 'Lossless File Compression',
-    tags: ['c', 'latex'],
-    code: 'https://github.com/xxzbuckxx/Huffman-Encoding',
-    description:
-      'A lossless static file compression program in C using a Huffman tree and a priority queue. This project was for a C programming lab I took in the spring quarter of my freshman year at UCSC.',
-  },
-  {
     title: 'Sorting Algorithms',
-    tags: ['c', 'bash', 'latex'],
-    code: 'https://github.com/xxzbuckxx/Sorting-Algorithms',
+    technologies: ['c', 'bash', 'latex'],
+    code: 'https://github.com/zacktraczyk/Sorting-Algorithms',
     previews: [sortingWriteup, sortingWriteupGraph, sortingComparisonGraph],
     description:
       'An implementations of bubble sort, shell sort, quicksort with a stack, and quicksort with a queue. Additionally, a writeup analyzing the produced algorithms was made with LaTeX, a bash script, and GNU Plot.',
   },
   {
-    text: 'I like making video games too. Here is one I am particularly proud of.',
-  },
-  {
-    title: "Ralph's Space Quest",
-    tags: ['gm2'],
-    demo: 'https://xxzbuckxx.itch.io/ralphs-space-quest-demo',
-    previews: [rsqTitle, rsqLevelSelect, rsqLevel, rsqBoss],
+    title: 'Lossless File Compression',
+    technologies: ['c', 'latex'],
+    code: 'https://github.com/zacktraczyk/Huffman-Encoding',
     description:
-      'A 2d-platformer shooter set on an extraterrestrial planet. Besides some help with a few visual assets, programming, design, and music was done by me.',
+      'A lossless static file compression program in C using a Huffman tree and a priority queue.',
   },
 ];
 
